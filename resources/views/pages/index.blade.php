955328a90204de3e073423421b3ad375fbc6be1b
Prishen Govender <36103415+PrishenG@users.noreply.github.com>
2018-06-17 02:44:01 +0200

    controller and views need to be completed

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">    
+        <h1>{{$title}}</h1>
+        <h5>Please Log In</h5>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Login</a></p>
+    </div>
+@endsection
